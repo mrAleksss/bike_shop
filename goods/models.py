158ab02eadfba72126dd0cb077d519957fe02fe8
Puhach.Developer <Puhach.dev@gmail.com>
2023-12-26 22:41:53 +0200

    add category and product models

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Назва')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'Категорію'
+        verbose_name_plural = 'Категорії'
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name='Назва')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
+    description = models.TextField(blank=True, null=True, verbose_name='Опис')
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Картинка')
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Ціна')
+    discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Знижка в %')
+    quantity = models.PositiveIntegerField(default=0, verbose_name='Кількість')
+    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='Категорія')
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукти'
