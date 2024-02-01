ffad458a597f91a4df0d3b3a3bec764e08b3a18d
Puhach.Developer <Puhach.dev@gmail.com>
2024-02-01 16:15:17 +0200

    cart remove controller added, commit before ajax

--- carts/urls.py
+++ carts/urls.py
@@ -6,5 +6,5 @@ app_name = 'carts'
 urlpatterns = [
     path('cart_add/<slug:product_slug>/', views.cart_add, name='cart_add'),
     path('cart_update/<slug:product_slug>/', views.cart_update, name='cart_update'),
-    path('cart_remove/<slug:product_slug>/', views.cart_remove, name='cart_remove'),
+    path('cart_remove/<int:cart_id>/', views.cart_remove, name='cart_remove'),
 ]
